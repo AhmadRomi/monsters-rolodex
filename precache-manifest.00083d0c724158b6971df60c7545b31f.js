140b4dba259245e4bbf833a781f959985f11c1cf
hamada romi <ahmed.romi2020@gmail.com>
2020-05-01 02:12:38 +0300

    Updates

---
+++ precache-manifest.00083d0c724158b6971df60c7545b31f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5301d4250056f1890d132b0993980d0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "634b386059b5f9458144",
+    "url": "/monsters-rolodex/static/css/main.59c05fbb.chunk.css"
+  },
+  {
+    "revision": "aac8c94dee885d426094",
+    "url": "/monsters-rolodex/static/js/2.504ce3f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.504ce3f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "634b386059b5f9458144",
+    "url": "/monsters-rolodex/static/js/main.0810bbac.chunk.js"
+  },
+  {
+    "revision": "4a06ec7ab72dfd821890",
+    "url": "/monsters-rolodex/static/js/runtime-main.b91b1c07.js"
+  }
+]);
